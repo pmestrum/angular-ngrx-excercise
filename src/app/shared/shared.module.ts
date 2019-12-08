8afa3ee6a7c4b352d55b777904f20ff73b41b8c1
Paul Mestrum <paul.mestrum@delaware.pro>
2019-12-08 12:27:09 +0100

    added bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PersonComponent } from './components/person/person.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
     PersonComponent
