e17bc3ac2e22ea8884db22aaf1c9ae8bb303c39f
Paul Mestrum <paul.mestrum@delaware.pro>
2021-11-28 20:23:44 +0100

    remove bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PersonComponent } from './components/person/person.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgbModule
   ],
   exports: [
     PersonComponent
