5f53a937f01d3934aed7791c1e5942228c9f4f65
Paul Mestrum <paul.mestrum@delaware.pro>
2021-11-29 11:35:29 +0100

    creating sandbox

--- src/app/app/person.sandbox.ts
+++ src/app/app/person.sandbox.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Person } from '../core/interfaces/person.interface';
+import { Store } from '@ngrx/store';
+import { first, Observable } from 'rxjs';
+import { Person, Persons } from '../core/interfaces/person.interface';
+import { PersonRestService } from '../core/services/person.rest.service';
+import { State } from '../core/interfaces/state.interface';
+import { personsLoadAction } from '../core/store/personState/person-actions';
 
 @Injectable()
 export class PersonSandbox {
 
   persons$: Observable<Person[]>; // TODO implement
 
+  constructor(private store: Store<State>, private personRestService: PersonRestService) {
+  }
+
   loadPersons(): void {
     // TODO implement
+
+    this.store.dispatch(personsLoadAction());
+
+    this.personRestService.getUsers$().pipe(
+      first(),
+    ).subscribe(
+      {
+        next: (persons: Persons) => {
+          // TODO dispatch success
+        },
+        error: (error) => {
+          // TODO dispatch fail
+        }
+      }
+    );
   }
 }
