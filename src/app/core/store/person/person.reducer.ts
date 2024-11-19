4ef91441e5da4c46d82f7eae12331c3c0629a014
Mestrum, Paul <Paul.Mestrum@delaware.pro>
2024-11-19 22:58:28 +0100

    some fixes

--- src/app/core/store/person/person.reducer.ts
+++ src/app/core/store/person/person.reducer.ts
@@ -1,13 +1,14 @@
-import { PersonState } from '../../interfaces/Person';
+import { Person } from '../../interfaces/Person';
 import { createReducer, on } from '@ngrx/store';
 import { personLoadAction, personLoadFailAction, personLoadSuccessAction } from './person.actions';
+import { PersonState } from '../../interfaces/state.interface';
 
 const INITIAL_STATE: PersonState = {
   loading: false,
   loaded: false,
   failed: false,
   errorMessage: '',
-  person: null
+  persons: [],
 };
 
 
@@ -18,24 +19,24 @@ function personLoadReducer() {
   });
 }
 
-function personLoadSuccessReducer() {
-  return (state, { person }) => ({
+function personLoadSuccessReducer(state: PersonState, payload: { persons: Person[] }) {
+  return {
     ...INITIAL_STATE,
     loaded: true,
-    person
-  });
+    persons: payload.persons
+  };
 }
 
-function personLoadFailReducer() {
-  return (state, { errorMessage }) => ({
+function personLoadFailReducer(state: PersonState, payload: { errorMessage: string }) {
+  return {
     ...INITIAL_STATE,
     failed: true,
-    errorMessage
-  });
+    errorMessage: payload.errorMessage
+  };
 }
 
 export const personReducer = createReducer(INITIAL_STATE,
   on(personLoadAction, personLoadReducer()),
-  on(personLoadSuccessAction, personLoadSuccessReducer()),
-  on(personLoadFailAction, personLoadFailReducer()),
+  on(personLoadSuccessAction, personLoadSuccessReducer),
+  on(personLoadFailAction, personLoadFailReducer),
 );
